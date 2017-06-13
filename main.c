928346c9e323a3dee20900087aaeb7598b254ded
explodi <phaser@tubatuba.net>
2017-06-12 22:20:26 -0400

    structs

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+struct visita {
+    char url[64];
+};
+
+struct nodo {
+    struct visita data; /* la visita */
+    struct nodo* next; /* el proximo */
+};
+
 int main()
 {
     printf("Hello World");
